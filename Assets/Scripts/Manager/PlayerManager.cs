21cc08a8143ca310e38fa1f3c9e04eb297b80fb0
duatv <duatv@sandinh.net>
2020-05-11 16:38:23 +0700

    fixxx

--- Assets/Scripts/Manager/PlayerManager.cs
+++ Assets/Scripts/Manager/PlayerManager.cs
@@ -20,7 +20,8 @@ public class PlayerManager : MonoBehaviour
     [SpineAnimation]
     public string str_idle, str_Win, str_Lose, str_Move;
     public PlayerDetectGems _detectGems;
-    public LayerMask lmColl;
+
+    public LayerMask lmColl, lmMapObject;
     public SkeletonAnimation saPlayer;
     public Rigidbody2D _rig2D;
     public float moveSpeed;
@@ -29,11 +30,11 @@ public class PlayerManager : MonoBehaviour
 
     [HideInInspector]
     public bool isContinueDetect = true;
-    private bool beginMove = false;
+    [HideInInspector]public bool beginMove = false;
     private bool isMoveLeft = false;
     private bool isMoveRight = false;
 
-    private RaycastHit2D hit2D;
+    private RaycastHit2D hit2D, hitDown;
     private Vector3 vEnd, vStart;
 
     private void Awake()
@@ -45,28 +46,7 @@ public class PlayerManager : MonoBehaviour
         GameManager.Instance.gTargetFollow = gameObject;
     }
 
-    private void PrepareMoveLeft()
-    {
-        if (pState != P_STATE.DIE)
-        {
-            beginMove = true;
-            saPlayer.skeleton.ScaleX = -1;
-            isMoveLeft = true;
-            isMoveRight = false;
-            PlayAnim(str_Move, true);
-        }
-    }
-    private void PrepareMoveRight()
-    {
-        if (pState != P_STATE.DIE)
-        {
-            beginMove = true;
-            saPlayer.skeleton.ScaleX = 1;
-            isMoveLeft = false;
-            isMoveRight = true;
-            PlayAnim(str_Move, true);
-        }
-    }
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.LeftArrow))
@@ -82,14 +62,24 @@ public class PlayerManager : MonoBehaviour
             beginMove = false;
             PlayAnim(str_idle, true);
         }
+        if (MapLevelManager.Instance.trTarget != null)
+        {
+            if (transform.localPosition.x > MapLevelManager.Instance.trTarget.localPosition.x)
+            {
+                saPlayer.skeleton.ScaleX = -1;
+            }
+            else
+            {
+                saPlayer.skeleton.ScaleX = 1;
+            }
+        }
     }
     private bool _isCanMoveToTarget;
     private void CheckHitAhead()
     {
         vStart = new Vector3(transform.localPosition.x + saPlayer.skeleton.ScaleX * 0.35f, transform.localPosition.y - 1.5f, transform.localPosition.z);
         vEnd = new Vector3(vStart.x + saPlayer.skeleton.ScaleX * 2f, vStart.y, vStart.z);
-        Debug.DrawLine(vStart, vEnd, Color.red);
-
+        Debug.DrawLine(vStart, vEnd, Color.yellow);
         hit2D = Physics2D.Linecast(vStart, vEnd, lmColl);
         if (hit2D.collider != null)
         {
@@ -102,20 +92,40 @@ public class PlayerManager : MonoBehaviour
                 {
                     _isCanMoveToTarget = true;
                 }
-            }else _isCanMoveToTarget = false;
+            }
+            else if (hit2D.collider.gameObject.GetComponent<StickBarrier>() != null) {
+                _isCanMoveToTarget = false;
+            }
+            else _isCanMoveToTarget = false;
         }
     }
     private void MoveToTarget()
     {
-        Debug.LogError(gameObject.name + " <<<<<<<<<< ");
-        _rig2D.velocity = moveSpeed * (saPlayer.skeleton.ScaleX > 0 ? Vector2.right : Vector2.left);
+        if (GameManager.Instance.gameState == GameManager.GAMESTATE.PLAYING)
+        {
+            PlayAnim(str_Move, true);
+            _rig2D.velocity = moveSpeed * (saPlayer.skeleton.ScaleX > 0 ? Vector2.right : Vector2.left);
+        }
     }
+    Vector3 _vStart, _vEnd;
+    private void HitDownMapObject() {
+         _vStart = new Vector3(transform.localPosition.x , transform.localPosition.y - 1.5f, transform.localPosition.z);
+         _vEnd = new Vector3(_vStart.x , _vStart.y - 0.15f, _vStart.z);
+        hitDown = Physics2D.Linecast(_vStart, _vEnd, lmMapObject);
+    }
+         
 
     private void FixedUpdate()
     {
         CheckHitAhead();
+        HitDownMapObject();
 
-        if (_isCanMoveToTarget) MoveToTarget();
+        if (_isCanMoveToTarget)
+        {
+            if (hitDown.collider != null)
+                if (hitDown.collider.gameObject.tag.Contains(Utils.TAG_WALL_BOTTOM))
+                    MoveToTarget();
+        }
 
 
         if (!IsCanMove()) _rig2D.velocity = Vector2.zero;
@@ -123,6 +133,7 @@ public class PlayerManager : MonoBehaviour
         {
             if (beginMove)
             {
+                Debug.LogError("1");
                 if (isMoveLeft) MoveLeft();
                 else if (isMoveRight) MoveRight();
             }
@@ -144,6 +155,47 @@ public class PlayerManager : MonoBehaviour
         if (pState != P_STATE.DIE)
             _rig2D.velocity = Vector2.right * moveSpeed;
         else _rig2D.velocity = Vector2.zero;
+    }
+
+    public void PrepareMoveLeft()
+    {
+        Debug.LogError("2");
+        if (pState != P_STATE.DIE)
+        {
+            beginMove = true;
+            saPlayer.skeleton.ScaleX = -1;
+            isMoveLeft = true;
+            isMoveRight = false;
+            PlayAnim(str_Move, true);
+        }
+    }
+    public void PrepareMoveRight()
+    {
+        Debug.LogError("3");
+        if (pState != P_STATE.DIE)
+        {
+            beginMove = true;
+            saPlayer.skeleton.ScaleX = 1;
+            isMoveLeft = false;
+            isMoveRight = true;
+            PlayAnim(str_Move, true);
+        }
+    }
+    public void PrepareRotate(Transform _trTarget, bool rotateOnly)
+    {
+        Debug.LogError("4");
+        if (transform.localPosition.x > _trTarget.localPosition.x)
+        {
+            saPlayer.skeleton.ScaleX = -1;
+            //if(!rotateOnly)
+                PrepareMoveLeft();
+        }
+        else
+        {
+            saPlayer.skeleton.ScaleX = 1;
+            //if (!rotateOnly)
+                PrepareMoveRight();
+        }
     }
     #endregion
 
@@ -191,29 +243,28 @@ public class PlayerManager : MonoBehaviour
                 beginMove = false;
                 PlayAnim(str_idle, true);
             }
-            if (MapLevelManager.Instance.questType == MapLevelManager.QUEST_TYPE.SAVE_HOSTAGE)
-            {
-                if (collision.gameObject.GetComponent<HostageManager>() != null)
-                {
-                    _rig2D.velocity = Vector2.zero;
-                    beginMove = false;
-                    GameManager.Instance.gameState = GameManager.GAMESTATE.WIN;
-                    if (GameManager.Instance.gameState != GameManager.GAMESTATE.LOSE) {
-                        collision.gameObject.GetComponent<HostageManager>().PlayWin();
-                        StartCoroutine(IEWin());
-                    }
-                }
-            }
+            //if (MapLevelManager.Instance.questType == MapLevelManager.QUEST_TYPE.SAVE_HOSTAGE)
+            //{
+            //    if (collision.gameObject.GetComponent<HostageManager>() != null)
+            //    {
+            //        _rig2D.velocity = Vector2.zero;
+            //        beginMove = false;
+            //        GameManager.Instance.gameState = GameManager.GAMESTATE.WIN;
+            //        if (GameManager.Instance.gameState != GameManager.GAMESTATE.LOSE) {
+            //            collision.gameObject.GetComponent<HostageManager>().PlayWin();
+            //            StartCoroutine(IEWin());
+            //        }
+            //    }
+            //}
             if (collision.gameObject.tag.Contains(Utils.TAG_WIN))
             {
-                Debug.LogError("!!");
-                _rig2D.velocity = Vector2.zero;
-                GameManager.Instance.gameState = GameManager.GAMESTATE.WIN;
-                beginMove = false;
-                if (GameManager.Instance.gameState != GameManager.GAMESTATE.LOSE)
-                {
-                    StartCoroutine(IEWin());
-                }
+                //_rig2D.velocity = Vector2.zero;
+                //GameManager.Instance.gameState = GameManager.GAMESTATE.WIN;
+                //beginMove = false;
+                //if (GameManager.Instance.gameState != GameManager.GAMESTATE.LOSE)
+                //{
+                //    StartCoroutine(IEWin());
+                //}
             }
         }
     }
@@ -252,30 +303,21 @@ public class PlayerManager : MonoBehaviour
             {
                 if (MapLevelManager.Instance.trTarget != null && MapLevelManager.Instance.trTarget.gameObject.activeSelf)
                 {
-                    PrepareRotate(MapLevelManager.Instance.trTarget);
+                    PrepareRotate(MapLevelManager.Instance.trTarget,false);
+                    Debug.LogError("6");
                 }
             }
         }
     }
 
-    private void PrepareRotate(Transform _trTarget)
+
+    public IEnumerator IEWin()
     {
-        if (transform.localPosition.x > _trTarget.localPosition.x)
-        {
-            saPlayer.skeleton.ScaleX = -1;
-            PrepareMoveLeft();
-        }
-        else
-        {
-            saPlayer.skeleton.ScaleX = 1;
-            PrepareMoveRight();
-        }
-    }
-    IEnumerator IEWin()
-    {
+        Debug.LogError("IEWin");
         GameManager.Instance.gameState = GameManager.GAMESTATE.WIN;
         pState = P_STATE.WIN;
         _rig2D.velocity = Vector2.zero;
+        beginMove = false;
         yield return new WaitForSeconds(1.0f);
         MapLevelManager.Instance.OnWin();
         PlayAnim(str_Win, true);
@@ -299,7 +341,8 @@ public class PlayerManager : MonoBehaviour
     }
     private void PlayAnim(string anim_, bool isLoop)
     {
-        saPlayer.AnimationState.SetAnimation(0, anim_, isLoop);
+        if(!saPlayer.AnimationName.Equals(anim_))
+            saPlayer.AnimationState.SetAnimation(0, anim_, isLoop);
     }
 }
 //#region Editor Mode
--- Assets/Scripts/PlayerDetectGems.cs
+++ Assets/Scripts/PlayerDetectGems.cs
@@ -10,8 +10,21 @@ public class PlayerDetectGems : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag.Contains(Utils.TAG_WIN)) {
-            if(gTarget != null)
+            if (gTarget == null)
+            {
                 gTarget = collision.gameObject;
+                pManager.PrepareRotate(transform, false);
+                //if (pManager.gameObject.transform.localPosition.x < transform.localPosition.x)
+                //{
+                //    pManager.saPlayer.skeleton.ScaleX = -1;
+                //    pManager.PrepareMoveLeft();
+                //}
+                //else
+                //{
+                //    pManager.saPlayer.skeleton.ScaleX = 1;
+                //    pManager.PrepareMoveRight();
+                //}
+            }
         }
     }
 }
