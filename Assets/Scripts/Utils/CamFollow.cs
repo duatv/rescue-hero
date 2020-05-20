eba87dab5f5b431c655f12e51c7d11533ccdcab4
duatv <duatv@sandinh.net>
2020-05-20 18:02:31 +0700

    fix gem bounder

--- Assets/Scripts/Utils/CamFollow.cs
+++ Assets/Scripts/Utils/CamFollow.cs
@@ -9,22 +9,27 @@ public class CamFollow : MonoBehaviour
     public float speed = 2.0f;
     public float camSize = 2.5f;
     public bool beginFollow;
+
+    Vector3 position;
+    float interpolation;
     void Update()
     {
-        if (beginFollow) {
-
+        if (beginFollow)
+        {
             if (Vector3.Distance(transform.position, objectToFollow.transform.position) == 10)
             {
                 beginFollow = false;
             }
-            else {
-                float interpolation = speed * Time.deltaTime;
+            else
+            {
+                interpolation = speed * Time.deltaTime;
                 _myCam.orthographicSize = camSize;
-                Vector3 position = this.transform.position;
+                position = this.transform.position;
                 position.y = Mathf.Lerp(this.transform.position.y, objectToFollow.transform.position.y, interpolation);
                 position.x = Mathf.Lerp(this.transform.position.x, objectToFollow.transform.position.x, interpolation);
                 this.transform.position = position;
             }
         }
     }
+    
 }
