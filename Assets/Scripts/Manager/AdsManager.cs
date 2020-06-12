695ff2cc5b9242a4bb104ec44e6de899776d0134
kiennt261193 <kiennt@sandinh.net>
2020-06-12 14:49:21 +0700

    clear all ads and firebase

--- Assets/Scripts/Manager/AdsManager.cs
+++ Assets/Scripts/Manager/AdsManager.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
-using GoogleMobileAds.Api.Mediation.UnityAds;
-using GoogleMobileAds.Api;
+//using GoogleMobileAds.Api.Mediation.UnityAds;
+//using GoogleMobileAds.Api;
 
 public class AdsManager : MonoBehaviour
 {
@@ -11,9 +11,9 @@ public class AdsManager : MonoBehaviour
     private Action<bool> acInterClosed, acRewarded;
 
     #region Admob
-    private InterstitialAd interstitial;
-    private RewardedAd rewardedAd;
-    private BannerView banner;
+    //private InterstitialAd interstitial;
+    //private RewardedAd rewardedAd;
+    //private BannerView banner;
     #endregion
 
     private void Awake()
@@ -28,41 +28,41 @@ public class AdsManager : MonoBehaviour
     }
     private void InitAds()
     {
-        MobileAds.Initialize(Application.platform == RuntimePlatform.Android ? Utils.APP_ID : Utils.APP_ID);
-        InitInterstitial();
-        InitRewarded();
-        InitBanner();
-        UnityAds.SetGDPRConsentMetaData(true);
+        //MobileAds.Initialize(Application.platform == RuntimePlatform.Android ? Utils.APP_ID : Utils.APP_ID);
+        //InitInterstitial();
+        //InitRewarded();
+        //InitBanner();
+    //    UnityAds.SetGDPRConsentMetaData(true);
     }
 
     private void InitBanner()
     {
-        banner = new BannerView(Application.platform == RuntimePlatform.Android ? Utils.BANNER_ID : Utils.BANNER_ID, AdSize.Banner, AdPosition.Bottom);
-        banner.OnAdLoaded += Banner_OnAdLoaded;
-        banner.LoadAd(CreateRequest());
+        //banner = new BannerView(Application.platform == RuntimePlatform.Android ? Utils.BANNER_ID : Utils.BANNER_ID, AdSize.Banner, AdPosition.Bottom);
+        //banner.OnAdLoaded += Banner_OnAdLoaded;
+        //banner.LoadAd(CreateRequest());
     }
     public void ShowBannerWithPos()
     {
-        if (banner != null)
-        {
-            banner.SetPosition(AdPosition.Bottom);
-            banner.Show();
-        }
+        //if (banner != null)
+        //{
+        //    banner.SetPosition(AdPosition.Bottom);
+        //    banner.Show();
+        //}
     }
     public void ShowBanner()
     {
-        if (banner != null)
-        {
-            banner.SetPosition(AdPosition.Bottom);
-            banner.Show();
-        }
+        //if (banner != null)
+        //{
+        //    banner.SetPosition(AdPosition.Bottom);
+        //    banner.Show();
+        //}
     }
     public void HideBanner()
     {
-        if (banner != null)
-        {
-            banner.Hide();
-        }
+        //if (banner != null)
+        //{
+        //    banner.Hide();
+        //}
     }
     private void Banner_OnAdLoaded(object sender, EventArgs e)
     {
@@ -71,12 +71,12 @@ public class AdsManager : MonoBehaviour
 
     private bool IsIntersLoaded()
     {
-        return interstitial.IsLoaded();
+        return /*interstitial.IsLoaded()*/false;
     }
 
     public bool IsRewardLoaded()
     {
-        return rewardedAd.IsLoaded();
+        return /*rewardedAd.IsLoaded()*/false;
     }
     public void ShowInterstitial(Action<bool> _ac)
     {
@@ -84,12 +84,12 @@ public class AdsManager : MonoBehaviour
             if (IsIntersLoaded())
             {
                 acInterClosed = _ac;
-                interstitial.Show();
+               // interstitial.Show();
 
             }
             else
             {
-                interstitial.LoadAd(CreateRequest());
+               // interstitial.LoadAd(CreateRequest());
                 if (acInterClosed != null)
                     acInterClosed(true);
             }
@@ -102,29 +102,29 @@ public class AdsManager : MonoBehaviour
         if (IsRewardLoaded())
         {
             acRewarded = _ac;
-            rewardedAd.Show();
+          //  rewardedAd.Show();
 
         }
         else
         {
-            rewardedAd.LoadAd(CreateRequest());
+          //  rewardedAd.LoadAd(CreateRequest());
             acRewarded(false);
         }
     }
 
 
     #region Init Admob
-    AdRequest CreateRequest()
-    {
-        AdRequest request = new AdRequest.Builder().AddTestDevice("BA730DD6C0C19894C11CB7FDF6D75AA8").AddTestDevice("D96EFB8D3BB99E5B5CAF739EB1EB5E9D").AddTestDevice("256FC58E8184F47CC4E7BE3570B2AC3B").Build();
-        return request;
-    }
+    //AdRequest CreateRequest()
+    //{
+    //    AdRequest request = new AdRequest.Builder().AddTestDevice("BA730DD6C0C19894C11CB7FDF6D75AA8").AddTestDevice("D96EFB8D3BB99E5B5CAF739EB1EB5E9D").AddTestDevice("256FC58E8184F47CC4E7BE3570B2AC3B").Build();
+    //    return request;
+    //}
     void InitInterstitial()
     {
-        interstitial = new InterstitialAd(Application.platform == RuntimePlatform.Android ? Utils.INTERS_ID : Utils.INTERS_ID);
-        interstitial.OnAdOpening += Interstitial_OnAdOpening;
-        interstitial.OnAdClosed += HandleOnAdClosed;
-        interstitial.LoadAd(CreateRequest());
+        //interstitial = new InterstitialAd(Application.platform == RuntimePlatform.Android ? Utils.INTERS_ID : Utils.INTERS_ID);
+        //interstitial.OnAdOpening += Interstitial_OnAdOpening;
+        //interstitial.OnAdClosed += HandleOnAdClosed;
+        //interstitial.LoadAd(CreateRequest());
     }
 
     private void Interstitial_OnAdOpening(object sender, EventArgs e)
@@ -134,13 +134,13 @@ public class AdsManager : MonoBehaviour
 
     void InitRewarded()
     {
-        this.rewardedAd = new RewardedAd(Application.platform == RuntimePlatform.Android ? Utils.VIDEO_ID : Utils.VIDEO_ID);
-        // Called when the user should be rewarded for interacting with the ad.
-        this.rewardedAd.OnUserEarnedReward += HandleUserEarnedReward;
-        this.rewardedAd.OnAdLoaded += RewardedAd_OnAdLoaded;
-        // Called when the ad is closed.
-        this.rewardedAd.OnAdClosed += HandleRewardedAdClosed;
-        this.rewardedAd.LoadAd(CreateRequest());
+        //this.rewardedAd = new RewardedAd(Application.platform == RuntimePlatform.Android ? Utils.VIDEO_ID : Utils.VIDEO_ID);
+        //// Called when the user should be rewarded for interacting with the ad.
+        //this.rewardedAd.OnUserEarnedReward += HandleUserEarnedReward;
+        //this.rewardedAd.OnAdLoaded += RewardedAd_OnAdLoaded;
+        //// Called when the ad is closed.
+        //this.rewardedAd.OnAdClosed += HandleRewardedAdClosed;
+        //this.rewardedAd.LoadAd(CreateRequest());
     }
 
 
@@ -150,7 +150,7 @@ public class AdsManager : MonoBehaviour
     {
         if (acInterClosed != null)
             acInterClosed(true);
-        interstitial.LoadAd(CreateRequest());
+     //   interstitial.LoadAd(CreateRequest());
     }
 
     private void RewardedAd_OnAdLoaded(object sender, EventArgs e)
@@ -166,13 +166,13 @@ public class AdsManager : MonoBehaviour
         InitRewarded();
     }
 
-    public void HandleUserEarnedReward(object sender, Reward args)
-    {
-        if (acRewarded != null)
-        {
-            acRewarded(true);
-        }
-    }
+    //public void HandleUserEarnedReward(object sender, Reward args)
+    //{
+    //    if (acRewarded != null)
+    //    {
+    //        acRewarded(true);
+    //    }
+    //}
     #endregion
     #endregion
 }
