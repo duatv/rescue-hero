9fea177f5f2bfbc83a7fd6ae49367f5b8c773aa2
kien269311 <kiennt@sandinh.net>
2020-06-16 12:03:24 +0700

    done all

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -137,7 +137,7 @@ public class GameManager : MonoBehaviour
         StartCoroutine(IEWaitToShowWinLose(true));
     }
     public int enemyKill;
-    int countpasslevel;
+    static int countpasslevel;
     private IEnumerator IEWaitToShowWinLose(bool isWin)
     {
         yield return new WaitForSeconds(0.5f);
@@ -183,12 +183,14 @@ public class GameManager : MonoBehaviour
                 }
                 if (DataController.instance != null)
                     DataController.instance.DoAchievment(4, enemyKill);
+
                 if (DataParam.firsttime == 0)
                 {
                     if (Utils.LEVEL_INDEX >= DataParam.levelpassshowad)
                     {
                         AdsManager.Instance.ShowInterstitial(null);
                         DataParam.firsttime = 1;
+                        Debug.LogError("========show ads TH 1");
                     }
                 }
                 else
@@ -200,6 +202,7 @@ public class GameManager : MonoBehaviour
                         DataParam.oldTimeShowAds = System.DateTime.Now;
                         AdsManager.Instance.ShowInterstitial(null);
                     }
+                    Debug.LogError("========show ads TH 2");
                 }
                 MyAnalytic.EventLevelCompleted(Utils.LEVEL_INDEX + 1);
             }
@@ -213,6 +216,7 @@ public class GameManager : MonoBehaviour
                 gPanelWin.gameObject.SetActive(true);
                 effectCamera.SetActive(false);
                 LoseDisplay();
+             //   countpasslevel = 0;
                 if (SoundManager.Instance != null)
                 {
                     SoundManager.Instance.PlaySound(SoundManager.Instance.acLose);
